2a5bf1e85d857f30ed4f3a32dd476272d3a63497
aimadtakhtoukh <atakhtoukh@excilys.com>
2015-03-16 10:02:02 +0100

    CLI finished, pagination started

--- src/cli/CommandLineGetter.java
+++ src/cli/CommandLineGetter.java
@@ -23,19 +23,32 @@ public class CommandLineGetter {
 		CREATE_COMPUTER("create") {
 			public void doAction(List<String> args) {
 				Computer c = new Computer();
+				if (args.size() < 4) {
+					System.out.println("Command form : create (computer_name) (introduction_date) (discontinued_date) (company)");
+					System.out.println("You can write null instead of a date.");
+					return;
+				}
 				c.setName(args.get(0));
 				LocalDateTime introduced;
-				try {
-					introduced = LocalDateTime.ofInstant(sdf.parse(args.get(1)).toInstant(), ZoneId.systemDefault());
-				} catch (ParseException e) {
+				if (args.get(1).equals("null")) {
 					introduced = null;
+				} else {
+					try {
+						introduced = LocalDateTime.ofInstant(sdf.parse(args.get(1)).toInstant(), ZoneId.systemDefault());
+					} catch (ParseException e) {
+						introduced = null;
+					}
 				}
 				c.setIntroduced(introduced);
 				LocalDateTime discontinued;
-				try {
-					discontinued = LocalDateTime.ofInstant(sdf.parse(args.get(2)).toInstant(), ZoneId.systemDefault());
-				} catch (ParseException e) {
+				if (args.get(2).equals("null")) {
 					discontinued = null;
+				} else {
+					try {
+						discontinued = LocalDateTime.ofInstant(sdf.parse(args.get(2)).toInstant(), ZoneId.systemDefault());
+					} catch (ParseException e) {
+						discontinued = null;
+					}
 				}
 				c.setDiscontinued(discontinued);
 				c.setCompanyId(Long.parseLong(args.get(3)));
@@ -48,11 +61,12 @@ public class CommandLineGetter {
 				for (String s : args) {
 					try {
 						Computer c1 = dao.getComputer(Long.parseLong(s));
-						System.out.println(c1.getId() + "\t" + c1.getName() + '\t' + c1.getIntroduced() + '\t' + c1.getDiscontinued());
+						System.out.println(c1.getId() + "\t" + c1.getName() + '\t' + c1.getIntroduced() + '\t' 
+								+ c1.getDiscontinued() + '\t' + c1.getCompanyId());
 					} catch (NullPointerException e) {
-						System.out.println("Cet ordinateur n'existe pas.");
+						System.out.println("That computer doesn't exist.");
 					} catch (NumberFormatException e) { 
-						System.out.println("Le deuxième argument doit être un nombre.");
+						System.out.println("Arguments must be numbers.");
 					}
 				}
 			}
@@ -60,26 +74,40 @@ public class CommandLineGetter {
 		READ_ALL_COMPUTERS("read_all") {
 			public void doAction(List<String> args) {
 				for (Computer c1 : dao.getAllComputers()) {
-					System.out.println(c1.getId() + "\t" + c1.getName() + '\t' + c1.getIntroduced() + '\t' + c1.getDiscontinued());
+					System.out.println(c1.getId() + "\t" + c1.getName() + '\t' + c1.getIntroduced() + '\t'
+							+ c1.getDiscontinued() + '\t' + c1.getCompanyId());
 				}				
 			}			
 		},
 		UPDATE_COMPUTER("update") {
 			public void doAction(List<String> args) {
+				if (args.size() < 5) {
+					System.out.println("Command form : update (id) (computer_name) (introduction_date) (discontinued_date) (company)");
+					System.out.println("You can write null instead of a date.");
+					return;
+				}
 				Computer c = new Computer();
 				c.setName(args.get(1));
 				LocalDateTime introduced;
-				try {
-					introduced = LocalDateTime.ofInstant(sdf.parse(args.get(2)).toInstant(), ZoneId.systemDefault());
-				} catch (ParseException e) {
+				if (args.get(2).equals("null")) {
 					introduced = null;
+				} else {
+					try {
+						introduced = LocalDateTime.ofInstant(sdf.parse(args.get(1)).toInstant(), ZoneId.systemDefault());
+					} catch (ParseException e) {
+						introduced = null;
+					}
 				}
 				c.setIntroduced(introduced);
 				LocalDateTime discontinued;
-				try {
-					discontinued = LocalDateTime.ofInstant(sdf.parse(args.get(3)).toInstant(), ZoneId.systemDefault());
-				} catch (ParseException e) {
+				if (args.get(3).equals("null")) {
 					discontinued = null;
+				} else {
+					try {
+						discontinued = LocalDateTime.ofInstant(sdf.parse(args.get(2)).toInstant(), ZoneId.systemDefault());
+					} catch (ParseException e) {
+						discontinued = null;
+					}
 				}
 				c.setDiscontinued(discontinued);
 				c.setCompanyId(Long.parseLong(args.get(4)));
@@ -106,7 +134,7 @@ public class CommandLineGetter {
 		ComputerDAO dao = ComputerDAOImpl.getInstance();
 		CompanyDAO companyDAO = CompanyDAOImpl.getInstance();
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YYYY", Locale.FRANCE);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.FRANCE);
 		
 		private String action;
 		ACTIONS(String a) {action = a;}
