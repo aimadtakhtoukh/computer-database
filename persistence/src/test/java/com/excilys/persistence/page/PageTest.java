2e2dd8be20bb8f62f6721018924b5cd58954ac31
aimadtakhtoukh <atakhtoukh@excilys.com>
2015-04-22 15:24:51 +0200

    Working Digest Auth Security. Now for some JAX-RS using RestEasy.

---
+++ persistence/src/test/java/com/excilys/persistence/page/PageTest.java
@@ -0,0 +1,114 @@
+package com.excilys.persistence.page;
+
+import org.dbunit.dataset.DataSetException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.excilys.persistence.dao.util.ComputerDatabaseConnectionFactory;
+import com.excilys.persistence.dao.util.DatabaseTestUtil;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class PageTest {
+
+	@Autowired
+	ComputerDatabaseConnectionFactory cdcf;
+	@Autowired
+	//ComputerService service;
+
+	@BeforeClass
+	public static void beforeClass() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		DatabaseTestUtil.setUpDatabase();
+	}
+	
+	@Before
+	public void prepareTestBase() throws DataSetException, Exception {
+		DatabaseTestUtil.executeSqlFile(
+				"test.sql", 
+				cdcf.getConnection());
+	}
+	
+	@After
+	public void cleanup() throws Exception {
+        DatabaseTestUtil.databaseTester.onTearDown();
+	}
+	
+	/*
+	@Test
+	public void aNewStandardPageHasALimitOf10() throws Exception {
+		//GIVEN
+		DatabaseTestUtil.cleanlyInsert(
+				new FlatXmlDataSetBuilder().build(new File(
+                "src/test/resources/datasets/computerDAO/get.xml")));
+		ComputerPage page = service.getComputerPage();
+		//WHEN
+		Assert.assertEquals(page.getLimit(), 10);
+		//THEN
+	}
+	
+	@Test
+	public void settingTheLimitOfThePageChangesIt() throws Exception {
+		DatabaseTestUtil.cleanlyInsert(
+				new FlatXmlDataSetBuilder().build(new File(
+                "src/test/resources/datasets/computerDAO/get.xml")));
+		ComputerPage page = service.getComputerPage();
+		//WHEN
+		page.setLimit(20);
+		Assert.assertEquals(page.getLimit(), 20);
+	}
+	
+	@Test
+	public void settingTheLimitofThePageToANegativeNumberSetsTheLimitToZero() throws Exception {
+		//GIVEN
+		DatabaseTestUtil.cleanlyInsert(
+				new FlatXmlDataSetBuilder().build(new File(
+                "src/test/resources/datasets/computerDAO/get.xml")));
+		ComputerPage page = service.getComputerPage();
+		//WHEN
+		page.setLimit(-1);
+		Assert.assertEquals(page.getLimit(), 0);
+	}
+	
+	@Test
+	public void aNewStandardPageHasAoffsetOf0() throws Exception {
+		//GIVEN
+		DatabaseTestUtil.cleanlyInsert(
+				new FlatXmlDataSetBuilder().build(new File(
+                "src/test/resources/datasets/computerDAO/get.xml")));
+		ComputerPage page = service.getComputerPage();
+		//WHEN
+		Assert.assertEquals(page.getOffset(), 0);
+		//THEN
+	}
+	
+	@Test
+	public void settingTheOfftOfThePageChangesIt() throws Exception {
+		//GIVEN
+		DatabaseTestUtil.cleanlyInsert(
+				new FlatXmlDataSetBuilder().build(new File(
+                "src/test/resources/datasets/computerDAO/get.xml")));
+		ComputerPage page = service.getComputerPage();
+		//WHEN
+		page.setOffset(20);
+		Assert.assertEquals(20, page.getOffset());
+	}
+	
+	@Test
+	public void settingTheOffsetofThePageToANegativeNumberSetTheValueToZero() throws Exception {
+		//GIVEN
+		DatabaseTestUtil.cleanlyInsert(
+				new FlatXmlDataSetBuilder().build(new File(
+                "src/test/resources/datasets/computerDAO/get.xml")));
+		ComputerPage page = service.getComputerPage();
+		//WHEN
+		page.setLimit(-1);
+		Assert.assertEquals(page.getLimit(), 0);
+	}
+	*/
+
+}
