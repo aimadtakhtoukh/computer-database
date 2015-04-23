d99a18c26afdcbc5010494cd54d78f9a94273ae0
aimadtakhtoukh <atakhtoukh@excilys.com>
2015-04-23 17:37:30 +0200

    List exposed using RestController. Computer Database project, the end!

---
+++ webapp/src/main/java/com/excilys/webapp/controller/rest/ComputerRestController.java
@@ -0,0 +1,31 @@
+package com.excilys.webapp.controller.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.excilys.core.beans.Computer;
+import com.excilys.service.services.ComputerService;
+
+@RestController
+@RequestMapping("/rest")
+public class ComputerRestController {
+	
+	@Autowired
+	private ComputerService service;
+
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+	public Computer getComputer(@PathVariable long id) {
+		return service.getComputer(id);
+	}
+	
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public List<Computer> getComputers() {
+		return service.getAll();
+	}
+	
+}
